6edae1a90fff4063bab17a3424257e7a659e3c8a
Jesus Luqui <jluqui97@gmail.com>
2022-03-22 23:23:46 -0300

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import { saludar } from './js/componentes.js';
 import './styles.css';
 
-const nombre = 'Carlos';
+import { Todo, TodoList } from './classes'
+import { crearTodoHtml } from './js/componentes';
+// import { Todo } from './classes/todo.class.js';
+// import { TodoList } from './classes/todo-list.class';
 
-saludar(nombre);
+export const todoList = new TodoList();
+
+todoList.todos.forEach( todo => crearTodoHtml(todo));
+
+console.log('todos', todoList.todos);
